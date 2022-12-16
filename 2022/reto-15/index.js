2812f79f32a213f498ebce90c8f3b0ce09553c09
qvacode <d3vqva@gmail.com>
2022-12-15 22:30:29 -0500

    refactors: reto 15 optimizado

--- 2022/reto-15/index.js
+++ 2022/reto-15/index.js
@@ -1,30 +1,25 @@
 export const decorateTree = (base) => {
-    const tree = base.split(' ')
-  
-    const getLetter = {
-      PP: 'P',
-      BB: 'B',
-      RR: 'R',
-      BP: 'R',
-      PB: 'R',
-      RP: 'B',
-      PR: 'B',
-      BR: 'P',
-      RB: 'P'
-    }
-  
-    return tree
-      .slice(1)
-      .reduce(
-        (acc, _, index) => {
-          const prevEl = acc[index]
-          const currEl = prevEl.slice(1).map((_, i) =>
-            getLetter[prevEl[i] + prevEl[i + 1]])
-  
-          return [...acc, currEl]
-        },
-        [tree]
-      )
-      .reverse()
-      .map(el => el.join(' '))
-  }
+  const tree = base.split(" ");
+
+  const getLetter = {
+    PP: "P",
+    BB: "B",
+    RR: "R",
+    BP: "R",
+    PB: "R",
+    RP: "B",
+    PR: "B",
+    BR: "P",
+    RB: "P",
+  };
+
+  const nextLevel = (el = []) => {
+    return el.slice(1).map((_, i) => getLetter[el[i] + el[i + 1]]);
+  };
+
+  return tree
+    .slice(1)
+    .reduce((acc, _, index) => [...acc, nextLevel(acc[index])], [tree])
+    .reverse()
+    .map((el) => el.join(" "));
+};
